e33c146fd0e86b1b4da2c5380c1fde401e8f69cf
techmate <techmate@okd4-svc-1.tech-mate.io>
2024-05-07 00:52:17 +0300

    added more entries to db.sql

--- db.sql
+++ db.sql
@@ -1,3 +1,6 @@
 USE items;
 CREATE TABLE Projects (id int NOT NULL, name varchar(255) DEFAULT NULL, code varchar(255) DEFAULT NULL, PRIMARY KEY (id));	
 INSERT INTO Projects (id, name, code) VALUES (1,'DevOps','DO180');
+INSERT INTO Projects (id, name, code) VALUES (1,'Alpha','Alpha-DO180');
+INSERT INTO Projects (id, name, code) VALUES (1,'Bravo','Bravo-DO180');
+INSERT INTO Projects (id, name, code) VALUES (1,'Charlie','Charlie-DO180');
